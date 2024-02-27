84d6e1978bfbb5e972a838ab2bc03eb105cf8bf6
Benjamin Yeager <benjaminyeager44@gmail.com>
2024-02-27 12:07:03 -0500

    Initial Commit

---
+++ FizzBuzz/FizzBuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  FizzBuzzApp.swift
+//  FizzBuzz
+//
+//  Created by Benjamin Yeager on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct FizzBuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
